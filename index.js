13f538e89d4e9feb4347e8e5e3d6c3f7a61fdbe2
Rafostar <40623528+Rafostar@users.noreply.github.com>
2019-09-14 21:40:53 +0200

    Initial version upload

---
+++ index.js
@@ -0,0 +1,70 @@
+const http = require('http');
+const url = require('url');
+const CecController = require('cec-controller');
+
+var cecCtl = new CecController({ osdString: 'CEC-WEB-API' });
+var ctlObj = {};
+var busy = false;
+
+cecCtl.once('ready', readyHandler);
+cecCtl.on('error', console.error);
+
+function readyHandler(controller)
+{
+	ctlObj = controller;
+
+	const server = http.createServer(requestListener);
+	const port = (process.argv[2] > 0 && process.argv[2] <= 65535) ? process.argv[2] : 8080;
+
+	server.listen(port);
+}
+
+function requestListener(req, res)
+{
+	var data = url.parse(req.url, true);
+	var result = getResult(data.pathname.split('/'));
+
+	if(result)
+	{
+		res.writeHead(200);
+
+		if(typeof result === 'function')
+		{
+			if(busy) return res.end('Previous function is still running!');
+
+			busy = true;
+			var value = (data.query) ? data.query.value : null;
+
+			return result(value).then(response =>
+			{
+				busy = false;
+
+				if(response === true) res.end('OK');
+				else if(response === null) res.end('ERROR');
+				else res.end(JSON.stringify(response));
+			});
+		}
+
+		return res.end(JSON.stringify(result));
+	}
+
+	res.writeHead(404);
+	res.end('Invalid Request!');
+}
+
+function getResult(keysArray)
+{
+	var result = Object.assign(ctlObj);
+
+	for(var key of keysArray)
+	{
+		if(!key) continue;
+
+		if(typeof result === 'object' && result[key])
+			result = result[key];
+		else
+			return null;
+	}
+
+	return result;
+}
